b0b4600dca87e752379ca25c6a773a1a7828159f
mvero-ext <mvero-ext@assystem.com>
2018-10-26 11:28:23 +0200

    Post Merge Commit, modifying the table view

--- TrackingTool/api/controllers/account/FileController.js
+++ TrackingTool/api/controllers/account/FileController.js
@@ -130,13 +130,18 @@ module.exports = {
   },
 
     identification_crawl: function(err, file){
-      
+      console.log("I am in the indentifacation crawl")
+      return file.filename;
     },
     pdf_crawl: function(err, file){
+      console.log("PDF Crawl")
+      return "<a href="+file.filename+">";
 
     },
     results_crawl: function(err, file){
-
+      console.log("I am in the results crawml function")
+      console.log(file.filename)
+      return file.filename;
     },
 
     upload: function(req, res) {
@@ -152,56 +157,11 @@ module.exports = {
       for(var i=0; i<pdf_field_list.length; i++){
         req.file(pdf_field_list[i]).upload(dirname, this.pdf_crawl)
       }
-          //Change according to local dirname
-          dirname: 'C:/Users/mvero-ext/Documents/GitHub/TrackingTool/TrackingTool/.tmp/uploads'
-        }, function(err, uploads) {
-          console.log(uploads)
-          if(uploads === undefined){ console.log('No Upload defined')
-          return res.serverError(err)}
+      if (err) { 
+        console.log('error uploading files')
+        return res.serverError(err) }       
 
-          if(uploads.length > 7) {
-            return res.send('Maximum 7 files can be uploaded')
-          }
-          //Upload multiple files
-          uploads.forEach(file => {
-            //Get doctype of uploaded file          
-            var data = file.filename.slice(file.filename.length-9,file.filename.length-4)
-            console.log(data) 
-            //Set doctype depending on the number
-            if(data.includes('1'))
-              {data = 'TabulatedResults'}
-            
-            if(data.includes('2'))
-              {data = 'Airline'}
-            
-            if(data.includes('3'))
-              {data = 'Fleet'}
-            
-            if(data.includes('4'))
-              {data = 'AircraftID'}
-            
-            if(data.includes('5'))
-              {data = 'Parameters'}
-            //Create each file with corresponding parameters
-            File.create({
-              path: file.fd,
-              filename: file.filename,
-              aircraft: file.filename.slice(0, file.filename.length-9),
-              doc: data        
-            }).exec(function(err, file) {              
-              if (err) 
-              { console.log('error in file')
-                return res.serverError(err)
-              }           
-          }); 
-        })          
-          if (err) { 
-            console.log('error uploading files')
-            return res.serverError(err) }       
-          //if (uploads.length === 0) { return res.badRequest('No file was uploaded') }
-
-          // if it was successful redirect and display all uploaded files
-            return res.redirect('/files');
-      
+      // if it was successful redirect and display all uploaded files
+        return res.redirect('/files');
     },
   }
