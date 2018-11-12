a71a330ee5be1a47ee4b97eb323dcae6ea4f3ccc
Victoria Masiero <masierovictoria@gmail.com>
2018-11-12 15:52:40 +0100

    requests view
    still working on the confirm button.

--- TrackingTool/config/routes.js
+++ TrackingTool/config/routes.js
@@ -14,26 +14,38 @@ module.exports.routes = {
   //  ╦ ╦╔═╗╔╗ ╔═╗╔═╗╔═╗╔═╗╔═╗
   //  ║║║║╣ ╠╩╗╠═╝╠═╣║ ╦║╣ ╚═╗
   //  ╚╩╝╚═╝╚═╝╩  ╩ ╩╚═╝╚═╝╚═╝
- 
-  'GET /':                  { action: 'view-homepage-or-redirect' },
 
-  'GET /welcome':           { view: 'pages/dashboard/welcome'},
+  'GET /': {
+    action: 'view-homepage-or-redirect'
+  },
 
-  '/upload':                { view: 'pages/account/upload-files'},
+  'GET /welcome': {
+    view: 'pages/dashboard/welcome'
+  },
 
-  'POST /account/file/upload':      'account/FileController.upload',
+  '/upload': {
+    view: 'pages/account/upload-files'
+  },
 
-  'POST /account/file/search':      'account/FileController.search',
+  'POST /account/file/upload': 'account/FileController.upload',
 
-  'POST /account/file/validate':    'account/FileController.validate',
+  'POST /account/file/search': 'account/FileController.search',
 
-  'GET /account/file/download/:id':    'account/FileController.download', // Download function disabled
-  
-  'POST /account/file/validate':      'account/FileController.validate',
+  'POST /account/file/validate': 'account/FileController.validate',
 
-  'POST /account/file/update/:id':        'account/FileController.update',
+  'GET /account/file/download/:id': 'account/FileController.download', // Download function disabled
+
+  'POST /account/file/validate': 'account/FileController.validate',
+
+  'POST /account/file/update/:id': 'account/FileController.update',
 
   'POST /account/file/edit': 'account/FileController.edit',
+
+  'GET /requests': {
+    //view: 'pages/account/view-requests',
+    controller: 'account/AdminController',
+    action: 'view'
+  },
 
   'GET /files': {
 
@@ -43,23 +55,52 @@ module.exports.routes = {
 
   },
 
-  'GET /faq':                { view:   'pages/faq' },
-  'GET /legal/terms':        { view:   'pages/legal/terms' },
-  'GET /legal/privacy':      { view:   'pages/legal/privacy' },
-  'GET /contact':            { view:   'pages/contact' },
+  'GET /faq': {
+    view: 'pages/faq'
+  },
+  'GET /legal/terms': {
+    view: 'pages/legal/terms'
+  },
+  'GET /legal/privacy': {
+    view: 'pages/legal/privacy'
+  },
+  'GET /contact': {
+    view: 'pages/contact'
+  },
 
-  'GET /signup':             { action: 'entrance/view-signup' },
-  'GET /email/confirm':      { action: 'entrance/confirm-email' },
-  'GET /email/confirmed':    { view:   'pages/entrance/confirmed-email' },
+  'GET /signup': {
+    action: 'entrance/view-signup'
+  },
+  'GET /email/confirm': {
+    action: 'entrance/confirm-email'
+  },
+  'GET /email/confirmed': {
+    view: 'pages/entrance/confirmed-email'
+  },
 
-  'GET /login':              { action: 'entrance/view-login' },
-  'GET /password/forgot':    { action: 'entrance/view-forgot-password' },
-  'GET /password/new':       { action: 'entrance/view-new-password' },
+  'GET /login': {
+    action: 'entrance/view-login'
+  },
+  'GET /password/forgot': {
+    action: 'entrance/view-forgot-password'
+  },
+  'GET /password/new': {
+    action: 'entrance/view-new-password'
+  },
 
-  'GET /account':            { action: 'account/view-account-overview' },
-  'GET /account/password':   { action: 'account/view-edit-password' },
-  'GET /account/profile':    { action: 'account/view-edit-profile' },
-  'GET /table':              { controller: "table", action: "view-available-data"},
+  'GET /account': {
+    action: 'account/view-account-overview'
+  },
+  'GET /account/password': {
+    action: 'account/view-edit-password'
+  },
+  'GET /account/profile': {
+    action: 'account/view-edit-profile'
+  },
+  'GET /table': {
+    controller: "table",
+    action: "view-available-data"
+  },
 
 
   //  ╔═╗╔═╗╦  ╔═╗╔╗╔╔╦╗╔═╗╔═╗╦╔╗╔╔╦╗╔═╗
@@ -67,14 +108,30 @@ module.exports.routes = {
   //  ╩ ╩╩  ╩  ╚═╝╝╚╝═╩╝╩  ╚═╝╩╝╚╝ ╩ ╚═╝
   // Note that, in this app, these API endpoints may be accessed using the `Cloud.*()` methods
   // from the CloudSDK library.
-  '/api/v1/account/logout':                           { action: 'account/logout' },
-  'PUT   /api/v1/account/update-password':            { action: 'account/update-password' },
-  'PUT   /api/v1/account/update-profile':             { action: 'account/update-profile' },
-  'PUT   /api/v1/entrance/login':                        { action: 'entrance/login' },
-  'POST  /api/v1/entrance/signup':                       { action: 'entrance/signup' },
-  'POST  /api/v1/entrance/send-password-recovery-email': { action: 'entrance/send-password-recovery-email' },
-  'POST  /api/v1/entrance/update-password-and-login':    { action: 'entrance/update-password-and-login' },
-  'POST  /api/v1/deliver-contact-form-message':          { action: 'deliver-contact-form-message' },
+  '/api/v1/account/logout': {
+    action: 'account/logout'
+  },
+  'PUT   /api/v1/account/update-password': {
+    action: 'account/update-password'
+  },
+  'PUT   /api/v1/account/update-profile': {
+    action: 'account/update-profile'
+  },
+  'PUT   /api/v1/entrance/login': {
+    action: 'entrance/login'
+  },
+  'POST  /api/v1/entrance/signup': {
+    action: 'entrance/signup'
+  },
+  'POST  /api/v1/entrance/send-password-recovery-email': {
+    action: 'entrance/send-password-recovery-email'
+  },
+  'POST  /api/v1/entrance/update-password-and-login': {
+    action: 'entrance/update-password-and-login'
+  },
+  'POST  /api/v1/deliver-contact-form-message': {
+    action: 'deliver-contact-form-message'
+  },
 
 
   //  ╦ ╦╔═╗╔╗ ╦ ╦╔═╗╔═╗╦╔═╔═╗
@@ -85,7 +142,7 @@ module.exports.routes = {
   //  ╔╦╗╦╔═╗╔═╗  ╦═╗╔═╗╔╦╗╦╦═╗╔═╗╔═╗╔╦╗╔═╗
   //  ║║║║╚═╗║    ╠╦╝║╣  ║║║╠╦╝║╣ ║   ║ ╚═╗
   //  ╩ ╩╩╚═╝╚═╝  ╩╚═╚═╝═╩╝╩╩╚═╚═╝╚═╝ ╩ ╚═╝
-  '/terms':                   '/legal/terms',
-  '/logout':                  '/api/v1/account/logout',
+  '/terms': '/legal/terms',
+  '/logout': '/api/v1/account/logout',
 
 };
