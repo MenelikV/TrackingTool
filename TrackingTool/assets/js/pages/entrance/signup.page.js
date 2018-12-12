121c3e69e358159513634c57ec3471c33ac5a8ae
Victoria <masierovictoria@gmail.com>
2018-12-12 11:35:25 +0100

    Show loader for file upload/signup

--- TrackingTool/assets/js/pages/entrance/signup.page.js
+++ TrackingTool/assets/js/pages/entrance/signup.page.js
@@ -9,7 +9,7 @@ parasails.registerPage('signup', {
     // For tracking client-side validation errors in our form.
     // > Has property set to `true` for each invalid property in `formData`.
     formErrors: { /* … */ },
- 
+
     // Syncing / loading state
     syncing: false,
 
@@ -23,11 +23,18 @@ parasails.registerPage('signup', {
   //  ╦  ╦╔═╗╔═╗╔═╗╦ ╦╔═╗╦  ╔═╗
   //  ║  ║╠╣ ║╣ ║  ╚╦╝║  ║  ║╣
   //  ╩═╝╩╚  ╚═╝╚═╝ ╩ ╚═╝╩═╝╚═╝
-  beforeMount: function() {
+  beforeMount: function () {
     // Attach any initial data from the server.
     _.extend(this, SAILS_LOCALS);
+
+    $(document).ready(function () {
+      $('#create').click(function () {
+        $('#load').show();
+      });
+    });
+
   },
-  mounted: async function() {
+  mounted: async function () {
     //…
   },
 
@@ -36,10 +43,11 @@ parasails.registerPage('signup', {
   //  ╩╝╚╝ ╩ ╚═╝╩╚═╩ ╩╚═╝ ╩ ╩╚═╝╝╚╝╚═╝
   methods: {
 
-    submittedForm: async function() {
-     
-        // If email confirmation is enabled, show the success message.
-        this.cloudSuccess = true;
+    submittedForm: async function () {
+
+      // If email confirmation is enabled, show the success message.
+
+      this.cloudSuccess = true;
       /*
       else {
         // Otherwise, redirect to the log-in dashboard.
@@ -50,36 +58,40 @@ parasails.registerPage('signup', {
       }*/
     },
 
-    handleParsingForm: function() {
+    handleParsingForm: function () {
       // Clear out any pre-existing error messages.
+
+
       this.formErrors = {};
 
       var argins = this.formData;
 
       // Validate full name:
-      if(!argins.fullName) {
+      if (!argins.fullName) {
         this.formErrors.fullName = true;
       }
 
       // Validate email:
-      if(!argins.emailAddress || !parasails.util.isValidEmailAddress(argins.emailAddress)) {
+      if (!argins.emailAddress || !parasails.util.isValidEmailAddress(argins.emailAddress)) {
         this.formErrors.emailAddress = true;
       }
 
       // Validate password:
-      if(!argins.password) {
+      if (!argins.password) {
         this.formErrors.password = true;
       }
 
       // Validate password confirmation:
-      if(argins.password && argins.password !== argins.confirmPassword) {
+      if (argins.password && argins.password !== argins.confirmPassword) {
         this.formErrors.confirmPassword = true;
       }
 
       // Validate ToS agreement:
-      if(!argins.agreed) {
+      if (!argins.agreed) {
         this.formErrors.agreed = true;
       }
+
+
 
       // If there were any issues, they've already now been communicated to the user,
       // so simply return undefined.  (This signifies that the submission should be
